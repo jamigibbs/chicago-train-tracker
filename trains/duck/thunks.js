44091f55264c4aa65027b8997d7eac11ed5ec7c1
Jami Gibbs <jami0821@gmail.com>
2018-06-01 16:05:22 -0500

    Refactoring train route thunks

--- trains/duck/thunks.js
+++ trains/duck/thunks.js
@@ -11,55 +11,41 @@ const GOOGLE_DIRECTIONS_URL = 'https://maps.googleapis.com/maps/api/directions/j
 const API_KEY = app.expo.ios.config.googleMapsApiKey
 const mode = 'transit'
 
-const fetchRedLineRoute = () => {
-  const origin = 'place_id:ChIJY_Cw_xTQD4gRM0qzmGJcCcU'
-  const destination = 'place_id:ChIJAxJFPeQlDogRXVsdIvn1WxM';
+const routeThunkCreator = (origin, destination, action) => {
   const url = `${GOOGLE_DIRECTIONS_URL}?origin=${origin}&destination=${destination}&key=${API_KEY}&mode=${mode}`;
 
   return async function(dispatch){
     try {
       const { data } = await axios.get(url)
       const decodedData = decode(data.routes[0].overview_polyline.points)
-      const action = gotRedLineRouteFromGoogle(decodedData)
-      dispatch(action)
+      const actionResponse = action(decodedData)
+      dispatch(actionResponse)
     } catch (err) { console.error(err) }
   }
+}
+
+const fetchRedLineRoute = () => {
+  const origin = 'place_id:ChIJY_Cw_xTQD4gRM0qzmGJcCcU'
+  const destination = 'place_id:ChIJAxJFPeQlDogRXVsdIvn1WxM'
+
+  return routeThunkCreator(origin, destination, gotRedLineRouteFromGoogle)
 }
 
 const fetchBrownLineRoute = () => {
   const origin = 'place_id:ChIJY5rFa-bND4gRpz7jFktCjns'
   const destination = 'place_id:ChIJU61QZbAsDogRU2ucgL2Me4U'
-  const url = `${GOOGLE_DIRECTIONS_URL}?origin=${origin}&destination=${destination}&key=${API_KEY}&mode=${mode}`
 
-  return async function(dispatch) {
-    try {
-      const { data } = await axios.get(url)
-      const decodedData = decode(data.routes[0].overview_polyline.points)
-      const action = gotBrownLineRouteFromGoogle(decodedData)
-      dispatch(action)
-    } catch (err) { console.error(err) }
-  }
+  return routeThunkCreator(origin, destination, gotBrownLineRouteFromGoogle)
 }
 
 const fetchBlueLineRoute = () => {
   const origin = 'place_id:EiYxMDAwIE9oYXJlIFdheSwgQ2hpY2FnbywgSUwgNjA2NjYsIFVTQQ'
   const destination = 'place_id:ChIJ611X0sEsDogRlHYw4Mv92JM'
-  const url = `${GOOGLE_DIRECTIONS_URL}?origin=${origin}&destination=${destination}&key=${API_KEY}&mode=${mode}`
 
-  return async function(dispatch) {
-    try {
-      const { data } = await axios.get(url)
-      const decodedData = decode(data.routes[0].overview_polyline.points)
-      const action = gotBlueLineRouteFromGoogle(decodedData)
-      dispatch(action)
-    } catch (err) { console.error(err) }
-  }
+  return routeThunkCreator(origin, destination, gotBlueLineRouteFromGoogle)
 }
 
 export default {
-  gotRedLineRouteFromGoogle,
-  gotBrownLineRouteFromGoogle,
-  gotBlueLineRouteFromGoogle,
   fetchRedLineRoute,
   fetchBrownLineRoute,
   fetchBlueLineRoute
